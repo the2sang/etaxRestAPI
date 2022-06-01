f82777faf701626dc24f136d8b6494e49170292f
the2sang <the2sang@gmail.com>
2022-06-01 21:21:56 +0900

    first commit

---
+++ src/test/java/com/kepco/etax/api/EtaxRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kepco.etax.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EtaxRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
