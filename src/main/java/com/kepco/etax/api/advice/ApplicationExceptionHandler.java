b5e6ceeb60996f5f59a0a3ff951527bb6e824de4
KHC <the2sang@gmail.com>
2022-07-16 04:44:31 +0900

    Add Example Exception Handle User

--- src/main/java/com/kepco/etax/api/advice/ApplicationExceptionHandler.java
+++ src/main/java/com/kepco/etax/api/advice/ApplicationExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.kepco.etax.api.advice;
 
+import com.kepco.etax.api.exception.UserNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -20,6 +21,15 @@ public class ApplicationExceptionHandler {
         ex.getBindingResult().getFieldErrors().forEach(error -> {
             errorMap.put(error.getField(), error.getDefaultMessage());
         });
+        return errorMap;
+    }
+
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler(UserNotFoundException.class)
+    public Map<String, String> handleBusinessException(UserNotFoundException ex) {
+        Map<String, String> errorMap = new HashMap<>();
+
+        errorMap.put("errorMessage", ex.getMessage());
         return errorMap;
     }
 }
