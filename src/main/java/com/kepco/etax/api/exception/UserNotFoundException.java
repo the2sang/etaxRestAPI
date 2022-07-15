13911b32a9dbc133e4294f0fba3c5b5bd79a99af
KHC <the2sang@gmail.com>
2022-07-16 04:34:51 +0900

    Add Example Exception Handle User

---
+++ src/main/java/com/kepco/etax/api/exception/UserNotFoundException.java
@@ -0,0 +1,7 @@
+package com.kepco.etax.api.exception;
+
+public class UserNotFoundException  extends Exception{
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/kepco/etax/api/service/UserService.java
@@ -0,0 +1,36 @@
+package com.kepco.etax.api.service;
+
+import com.kepco.etax.api.domain.entity.User;
+import com.kepco.etax.api.domain.request.UserRequest;
+import com.kepco.etax.api.exception.UserNotFoundException;
+import com.kepco.etax.api.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+   @Autowired
+   private UserRepository repository;
+
+   public User saveUser(UserRequest userRequest) {
+       User user = User.build(0, userRequest.getName(), userRequest.getEmail(), userRequest.getMobile(),
+               userRequest.getGender(), userRequest.getAge(), userRequest.getNationality());
+       return repository.save(user);
+   }
+
+   public List<User> getAllUsers() {
+       return repository.findAll();
+   }
+
+   public User getUser(int id) throws UserNotFoundException {
+       User user = repository.findByUserId(id);
+       if (user!=null) {
+           return user;
+       } else {
+           throw new UserNotFoundException("user not found with id:" + id);
+       }
+   }
+}
