0e697b3afd93f134d75762d3f7ba27ab69791333
KHC <the2sang@gmail.com>
2022-07-16 16:06:14 +0900

    Add JWT Authentication

--- src/test/java/com/kepco/etax/api/EtaxRestApiApplicationTests.java
+++ src/test/java/com/kepco/etax/api/EtaxRestApiApplicationTests.java
@@ -3,7 +3,7 @@ package com.kepco.etax.api;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class EtaxRestApiApplicationTests {
 
     @Test
