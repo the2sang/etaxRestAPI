af5b29fc20d88250f8a47044c96dc68c0fbc35a8
KHC <the2sang@gmail.com>
2022-07-22 23:14:43 +0900

    Add TaxDaemon Entity Class

--- src/main/java/com/kepco/etax/api/EtaxRestApiApplication.java
+++ src/main/java/com/kepco/etax/api/EtaxRestApiApplication.java
@@ -8,7 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EtaxRestApiApplication {
 
-
     private static final Logger logger = LoggerFactory.getLogger(EtaxRestApiApplication.class);
     public static void main(String[] args) {
 
