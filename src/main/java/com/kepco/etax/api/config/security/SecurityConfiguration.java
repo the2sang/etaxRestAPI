ea55841469d07f5cc9602cc14fb5e864ac55f9c2
KHC <the2sang@gmail.com>
2022-07-24 02:04:17 +0900

    Update JWT User, Role, Refresh

--- src/main/java/com/kepco/etax/api/config/security/SecurityConfiguration.java
+++ src/main/java/com/kepco/etax/api/config/security/SecurityConfiguration.java
@@ -36,10 +36,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         http
                 .httpBasic().disable()
                 .csrf().disable()
-
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-
                 .and()
                 .authorizeRequests()
              //    .antMatchers(HttpMethod.POST, "/api/v1/login").permitAll()
@@ -50,9 +48,6 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 // Admin
                 .antMatchers(  "/api/v1/users").hasRole("ADMIN")
                 .anyRequest().authenticated()
-                //.anyRequest().hasRole("USER")
-//                .antMatchers(HttpMethod.POST, "/api/v1/createUser", "/exception/**").permitAll()
-//                .anyRequest().hasRole("ADMIN")
                 .and()
                 .exceptionHandling()
                 .authenticationEntryPoint(customAuthenticationEntryPoint)
@@ -64,7 +59,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     public void configure(WebSecurity web) throws Exception {
         web.ignoring().antMatchers("/api/v1/login", "/api/v1/createUser","/v2/api-docs", "/swagger-resources/**",
-                "/swagger-ui.html", "/webjars/**", "/swagger/**", "/h2-console/**");
+                "/swagger-ui.html", "/webjars/**", "/swagger/**");
     }
 
     @Bean
